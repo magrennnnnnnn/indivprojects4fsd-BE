6c1b4dfb53ccbb745b724c03584dd00412eaf3ea
magrennn <mmagrenturcu20@gmail.com>
2026-03-05 22:30:58 +0100

    i think this time is ok

---
+++ ProLinkBE/src/main/java/com/prolink/prolink/ProLinkBeApplication.java
@@ -0,0 +1,13 @@
+package com.prolink.prolink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProLinkBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProLinkBeApplication.class, args);
+    }
+
+}
---
+++ ProLinkBE/src/test/java/com/prolink/prolink/ProLinkBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prolink.prolink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProLinkBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
