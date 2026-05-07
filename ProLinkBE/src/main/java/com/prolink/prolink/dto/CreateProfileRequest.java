d944110a982a4bcd637fce6972bcf4fe4da35d27
magrennn <mmagrenturcu20@gmail.com>
2026-05-07 11:01:55 +0200

    dto with validation(only for profile fow now)

--- ProLinkBE/src/main/java/com/prolink/prolink/dto/CreateProfileRequest.java
+++ ProLinkBE/src/main/java/com/prolink/prolink/dto/CreateProfileRequest.java
@@ -1,8 +1,18 @@
 package com.prolink.prolink.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class CreateProfileRequest {
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 100)
     private String name;
+
+    @NotBlank(message = "Location is required")
     private String location;
+
+    @NotBlank(message = "Personal details are required")
+    @Size(min = 10, max = 2000)
     private String personalDetails;
 
     public CreateProfileRequest() {}
